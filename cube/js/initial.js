5229c1d5e142386580631ad4138b00302e609691
loho <39108845+loho1968@users.noreply.github.com>
2023-07-03 16:31:27 +0800

    loho

--- cube/js/initial.js
+++ cube/js/initial.js
@@ -2,15 +2,15 @@
 
 	TWIST NOTATION
 
-	UPPERCASE = Clockwise to next 90 degree peg
-	lowercase = Anticlockwise to next 90 degree peg
+	UPPERCASE = Clockwise to next 90 degree peg 顺时针至下一个90度桩
+	lowercase = Anticlockwise to next 90 degree peg 逆时针至下一个90度销钉
 
 
 
-	FACE & SLICE ROTATION COMMANDS
+	FACE & SLICE ROTATION COMMANDS 面和切片旋转命令
 
 	F	Front
-	S 	Standing (rotate according to Front Face's orientation)
+	S 	Standing (rotate according to Front Face's orientation) 站立（根据正面的方向旋转）
 	B 	Back
 
 	L 	Left
@@ -23,9 +23,9 @@
 
 
 
-	ENTIRE CUBE ROTATION COMMANDS
+	ENTIRE CUBE ROTATION COMMANDS 整个立方体旋转命令
 
-	X   Rotate entire cube according to Right Face's orientation
+	X   Rotate entire cube according to Right Face's orientation 根据右面的方向旋转整个立方体
 	Y   Rotate entire cube according to Up Face's orientation
 	Z   Rotate entire cube according to Front Face's orientation
 
@@ -58,7 +58,6 @@ $(document).ready(function() {
 
 	const container = document.getElementById( 'container' );
 	container.appendChild( cubeGL.domElement );
-
 	if (controls === ERNO.Locked) {
 		const fixedOrientation = new THREE.Euler(Math.PI * 0.1, Math.PI * -0.25, 0);
 		cubeGL.object3D.lookAt(cubeGL.camera.position);
@@ -70,7 +69,7 @@ $(document).ready(function() {
 	// The deviceMotion function provide some subtle mouse based motion
 	// The effect can be used with the Freeform and Locked controls.
 	// This could also integrate device orientation on mobile
-
+	//deviceMotion功能提供了一些基于鼠标的精细运动。该效果可与“自由形式”和“锁定”控件一起使用。这也可以在移动设备上集成设备定向
 	// var motion = deviceMotion( cubeGL, container );
 
 	// motion.decay = 0.1; 				// The drag effect
@@ -79,9 +78,11 @@ $(document).ready(function() {
 	// motion.range.z = 0;
 	// motion.paused = false;				// disables the effect
 
-	cubeGL.twistDuration = 300;
-	cubeGL.twist('xY');
-	//cubeGL.shuffle();
-	//cubeGL.twist('d');
-	//cubeGL.solve();
+	cubeGL.twistDuration = 300;//旋转速度
+
+	cubeGL.twist('X');//学习点：旋转
+
+	// cubeGL.shuffle(); //打乱
+	// cubeGL.twist('d');//旋转
+	// cubeGL.solve();//还原
 })
